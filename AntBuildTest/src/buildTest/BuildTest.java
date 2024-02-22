404a0fb13f826f84bac30195a3a6477b2577953d
User -2023CUFDE\Administrator <lsw940617@gmail.com>
2024-02-22 18:19:38 +0900

    Build(빌드)-ANT를 활용

---
+++ AntBuildTest/src/buildTest/BuildTest.java
@@ -0,0 +1,23 @@
+package buildTest;
+
+import com.ncs.green.domain.UserDTO;
+import com.ncs.green.service.DTOService;
+
+public class BuildTest {
+
+	public static void main(String[] args) {
+			
+			UserDTO dto = new UserDTO();
+			dto.setId("banana");
+			dto.setName("홍길동");
+			dto.setLoginTime("2023/02/22 AM 10:04:04");
+			
+			System.out.println(" ** 직접 출력 => "+dto);
+			
+			DTOService service = new DTOService();
+			service.setUserDTO(dto);
+			System.out.println(" ** AntBuild Test **" );
+			System.out.println(" ** DTOService => "+ service.getUserDTO());
+	}
+
+}
